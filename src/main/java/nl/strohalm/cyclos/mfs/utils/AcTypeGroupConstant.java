5254332100c46b3aa542591a8ac87ef99aaaa31f
bodrudduja-chowdhury <bodrudduja.chowdhury@redotdigitalit.com>
2023-03-07 13:27:58 +0600

    DO ac type group mapped

--- src/main/java/nl/strohalm/cyclos/mfs/utils/AcTypeGroupConstant.java
+++ src/main/java/nl/strohalm/cyclos/mfs/utils/AcTypeGroupConstant.java
@@ -8,10 +8,10 @@ public class AcTypeGroupConstant {
   public static  HashMap<AccountType,Long> acTypeGroups = new HashMap();
 
   static {
-    acTypeGroups.put(AccountType.CUSTOMER,5L);
-    acTypeGroups.put(AccountType.MERCHANT,12L);
-    acTypeGroups.put(AccountType.AGENT,13L);
-    acTypeGroups.put(AccountType.DISTRIBUTOR,14L);
-    acTypeGroups.put(AccountType.DO,15L);
+    acTypeGroups.put(AccountType.CUSTOMER,22L);
+    acTypeGroups.put(AccountType.MERCHANT,61L);
+    acTypeGroups.put(AccountType.AGENT,25L);
+    acTypeGroups.put(AccountType.DISTRIBUTOR,43L);
+    acTypeGroups.put(AccountType.DO,42L);
   }
 }
