5f69c4152e00188e79657cf1d491822e267e19e6
bodrudduja-chowdhury <bodrudduja.chowdhury@redotdigitalit.com>
2023-06-13 20:58:39 +0600

    Log4j upgraded

--- src/main/java/nl/strohalm/cyclos/mfs/controllers/TxnController.java
+++ src/main/java/nl/strohalm/cyclos/mfs/controllers/TxnController.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
--- src/main/java/nl/strohalm/cyclos/mfs/services/MfsAccountService.java
+++ src/main/java/nl/strohalm/cyclos/mfs/services/MfsAccountService.java
@@ -49,8 +49,8 @@ import nl.strohalm.cyclos.utils.Transactional;
 import nl.strohalm.cyclos.utils.validation.ValidationException;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
